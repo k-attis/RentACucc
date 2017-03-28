7a499205b607d7155eeb6df2a641a01a5857cea6
123 <123@123-PC>
2017-03-28 14:55:13 +0200

    FoLap és ahhoz gombok hozzáadása

--- RentACucc/RentACucc/App.cs
+++ RentACucc/RentACucc/App.cs
@@ -13,23 +13,7 @@ namespace RentACucc
 
         public App()
         {
-            // The root page of your application
-            var content = new ContentPage
-            {
-                Title = "RentACucc",
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
-
-            MainPage = new NavigationPage(content);
+           MainPage = new NavigationPage(new View.FoLap());
         }
 
         protected override void OnStart()
---
+++ RentACucc/RentACucc/View/FoLap.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace RentACucc.View
+{
+    public partial class FoLap : ContentPage
+    {
+        public FoLap()
+        {
+            InitializeComponent();
+
+            Cuccok.Clicked += Cuccok_Clicked;
+            Juserek.Clicked += Juserek_Clicked;
+            Kolcsik.Clicked += Kolcsik_Clicked;
+            LejartKolcsik.Clicked += LejartKolcsik_Clicked;
+        }
+
+        private void LejartKolcsik_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Kolcsik_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Juserek_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Cuccok_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
